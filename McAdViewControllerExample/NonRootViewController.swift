f6bf07d9b114218ac1e8806db6d7e99b34b34247
Kevin McGill <kevin@mcgilldevtech.com>
2017-05-04 22:14:23 -0500

    Add comment

--- McAdViewControllerExample/NonRootViewController.swift
+++ McAdViewControllerExample/NonRootViewController.swift
@@ -15,10 +15,13 @@ class NonRootViewController: UIViewController {
     @IBAction func displayInterstitial(_ sender: UIButton) {
         
         // From a View Controller that isn't in the view hierchy you'll need to pass the non-root view controller
+        // If you don't you'll get an error like:
+        //
+        // Attempt to present <GADInterstitialViewController: 0x7fbd47607880> on <McAdViewControllerExample.McAdViewController: 0x7fbd4760a8b0>
+        // whose view is not in the window hierarchy!
         app.mcAdViewController?.displayInterstitial(fromViewController: self)
     }
 
-    
     @IBAction func dismiss(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
