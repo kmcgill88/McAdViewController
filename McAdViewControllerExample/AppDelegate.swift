f30f6ac4a36382118beda818e25e4be4539a7ded
Kevin McGill <kevin@mcgilldevtech.com>
2018-10-26 12:46:53 -0500

    Spacing

--- McAdViewControllerExample/AppDelegate.swift
+++ McAdViewControllerExample/AppDelegate.swift
@@ -28,11 +28,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Test Ad units from https://developers.google.com/admob/ios/test-ads#sample_ad_units
         //
         mcAdViewController = McAdViewController(contentController: viewController, // Required
-            applicationId: "ca-app-pub-<YOUR APP ID>", // Required
-            bannerAdUnitId: "ca-app-pub-<YOUR BANNER ID>", // Conditional Optional - Required if interstantialAdUnitId not provided
-            interstantialAdUnitId: "ca-app-pub-<YOUR INT ID>", // Conditional Optional - Required if bannerAdUnitId not provided
-            isBannerBottom: false, // Optional - Default: true
-            debug: true) // Optional - Default: false
+                                                applicationId: "ca-app-pub-<YOUR APP ID>", // Required
+                                                bannerAdUnitId: "ca-app-pub-<YOUR BANNER ID>", // Conditional Optional - Required if interstantialAdUnitId not provided
+                                                interstantialAdUnitId: "ca-app-pub-<YOUR INT ID>", // Conditional Optional - Required if bannerAdUnitId not provided
+                                                isBannerBottom: false, // Optional - Default: true
+                                                debug: true) // Optional - Default: false
         
         mcAdViewController?.safeAreaColor = .red
         // Set McAdViewController as the root
